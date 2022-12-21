b5241265a058999cf8b3e811541c902dff51ddda
Verner-Ad <79918886+Verner-Ad@users.noreply.github.com>
2022-12-21 13:32:24 +0300

    Update script.js

--- web/5/script.js
+++ web/5/script.js
@@ -1,4 +1,5 @@
 "use strict";
+let name = prompt("What's your name?");
 function calc() {
     let form = document.forms.calculator;
     let a = form.elements.price;
@@ -13,14 +14,16 @@ function calc() {
     let b3 = form.elements.count3;
     let result = document.getElementById("result");
     let r = a.value * b.value + a0.value * b0.value + a1.value * b1.value + a2.value * b2.value + a3.value * b3.value;
-    if(isNaN(r)) {
-        result.innerHTML = "It isn't numbers";
-    }
-    else {
-        result.innerHTML = names + " , you have to pay: " + r + " rubles";
-    }
+let test=true;
+  if (a0.value<0||a1.value<0||a2.value<0||a3.value<0||a.value<0||b0.value<0||b1.value<0||b2.value<0||b3.value<0||b.value<0) test=false;
+  let result = document.getElementById("result");
+  if (isNaN(r)||test==false) result.innerHTML = "Что-то пошло не так...";
+  else {
+    result.innerHTML = `${name || "Аноним"}, с вас ${r} рублей.`;
+  }
 }
 window.addEventListener("DOMContentLoaded", function (event) {
-  console.log("DOM fully loaded and parsed");
-});
-let names = prompt("Input your name and last", "Dear customer");
+    console.log("DOM fully loaded and parsed");
+  });
+let btnn=document.getElementById("btn");
+btnn.addEventListener("click",calc);
