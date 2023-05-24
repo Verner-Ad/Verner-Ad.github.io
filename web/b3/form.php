1fe7e294cc81501d5b22c82d924f23a1db00818d
Verner-Ad <79918886+Verner-Ad@users.noreply.github.com>
2023-05-24 23:18:17 +0300

    Update form.php

--- web/b3/form.php
+++ web/b3/form.php
@@ -4,22 +4,22 @@ $error ="";
 // $_POST - один из суперглобальных массивов, инициализирован всегда
 if (empty($_POST['name'])) {
     // создаем HTTP header
-    error+="Name is not set\n";
+    $error+="Name is not set\n";
     // exit() завершает выполнение скрипта
 }
 // встроенная в php функция для валидации email
 if (empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
-       error += "Mail is not set or is invalid\n";
+       $error += "Mail is not set or is invalid\n";
 }
 if (empty($_POST['year'])) {
-   error+="Year is not set\n";
+   $error+="Year is not set\n";
 }
 if (empty($_POST['limbs'])) {
-   error+= "Limbs number is not set\n";
+   $error+= "Limbs number is not set\n";
 }
 // здесь применяем isset (проверка на NULL), т.к. empty выдаст true на '0', а у нас gender = {0, 1}
 if (!isset($_POST['gender']) || ($_POST['gender'] != 0 && $_POST['gender'] != 1)) {
-    error+="Gender is not set or is invalid\n";
+    $error+="Gender is not set or is invalid\n";
 }
 print($error);
 header("HTTP/1.1 400 ");
